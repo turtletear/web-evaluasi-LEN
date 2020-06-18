c4bb743affb6e07c7cdb3b4fd32c4c2269d057e9
ariqmusyaffaramadhani <ariqmr123@gmail.com>
2020-06-18 23:52:14 +0800

    page nilai atasan, ceil/floor kinerja ambigu, can't save

--- application/controllers/C_nilai_atasan.php
+++ application/controllers/C_nilai_atasan.php
@@ -17,6 +17,35 @@ class C_nilai_atasan extends CI_Controller {
 		$this->load->view('navbar');
 		$this->load->view('atasan/nilai_atasan',$dataEmp);
     }
+
+    public function convPoint($point)
+    {
+        if ($point == 5) {
+            $x = 6.7;
+        }
+        elseif ($point == 4){
+            $x = 5.3;
+        }
+        elseif ($point == 3){
+            $x = 4.0;
+        }
+        elseif ($point == 2){
+            $x = 2.7;
+        }
+        elseif ($point == 1){
+            $x = 1.3;
+        }
+
+        
+
+        return round($x, 2);
+    }//end func
+
+    public function ttlEval($kin)
+    {
+        $x = ($kin*70)/100;
+        return round($x, 0, PHP_ROUND_HALF_DOWN);
+    }
     
     public function addPenilaian()
     {
@@ -39,12 +68,64 @@ class C_nilai_atasan extends CI_Controller {
         if($this->form_validation->run() == false){
             $id_emp = 11;  //id karyawan dapat dari flashdata
             $dataEmp = $this->M_weblen->getDataKar($id_emp); //get data karyawan
+
+
+
             $this->load->view('navbar');
 			$this->load->view('atasan/nilai_atasan', $dataEmp);
 		
         } //end if
         else{
-            echo "anjay mabar";
+            $name = "Muten Roshi";  //dari flashdata / session
+            $nikA = 1201164155;
+
+            $aa = $this->input->post('inisiatif');
+            $bb = $this->input->post('kreatif');
+            $cc = $this->input->post('probsolv');
+            $dd = $this->input->post('tjawab');
+            $ee = $this->input->post('komitmen');
+            $ff = $this->input->post('etika');
+            $gg = $this->input->post('adaptasi');
+            $hh = $this->input->post('pelayanan');
+            $ii = $this->input->post('ktugas');
+            $jj = $this->input->post('penyesuaian');
+            $kk = $this->input->post('komunikasi');
+            $ll = $this->input->post('kerjasama');
+            $mm = $this->input->post('disiplin');
+            $nn = $this->input->post('skerja');
+            $oo = $this->input->post('hkerja');
+
+            // echo 'ini poin inisiatif ',$aa,' dikonversi jadi ',$this->convPoint($aa);
+            $kinerja = $this->convPoint($aa)+$this->convPoint($bb)+$this->convPoint($cc)+$this->convPoint($dd)+
+            $this->convPoint($ee)+$this->convPoint($ff)+$this->convPoint($gg)+$this->convPoint($hh)+$this->convPoint($ii)+
+            $this->convPoint($jj)+$this->convPoint($kk)+$this->convPoint($ll)+
+            $this->convPoint($mm)+$this->convPoint($nn)+$this->convPoint($oo) ;
+            $evl = $this->ttlEval($kinerja);
+            $now = date("Y-m-d");
+            $dataEvl = [
+                'date_fill' => $now,
+                'inisiatif' => $aa,
+                'daya_kreatif' => $bb,
+                'prob_solve' => $cc,
+                'tang_jawab' => $dd,
+                'kom_per' => $ee,
+                'etika_kerja' => $ff,
+                'adap_kerja' => $gg,
+                'pelayanan' => $hh,
+                'kem_tugas' => $ii,
+                'pen_diri' => $jj,
+                'kem_komunikasi' => $kk,
+                'ker_sama' => $ll,
+                'disiplin' => $mm,
+                'sis_kerja' => $nn,
+                'has_kerja' => $oo,
+                'nilai_eval' => $evl,
+                'nilai_kinerja' => round($kinerja, 0, PHP_ROUND_HALF_DOWN),
+                'nama_atasan' => $name,
+                'nik_atasan' => $nikA
+            ];
+
+            var_dump($dataEvl);
         }
     }
 	
